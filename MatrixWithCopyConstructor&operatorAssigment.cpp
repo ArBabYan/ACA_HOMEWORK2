bb21f0b4d6dea08dc9b0e964070f02d4a49425fa
ArBabYan <arame.babayan8@gmail.com>
2023-06-17 18:22:45 +0400

    MatrixWithCopyConstructor&operatorAssigment

---
+++ MatrixWithCopyConstructor&operatorAssigment.cpp
@@ -0,0 +1,31 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 17
+VisualStudioVersion = 17.6.33712.159
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "MatrixWithCopyConstructor&operatorAssigment", "MatrixWithCopyConstructor&operatorAssigment\MatrixWithCopyConstructor&operatorAssigment.vcxproj", "{1DEA8D1D-8BB9-4CC8-BBAD-8A3167940575}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|x64 = Debug|x64
+		Debug|x86 = Debug|x86
+		Release|x64 = Release|x64
+		Release|x86 = Release|x86
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{1DEA8D1D-8BB9-4CC8-BBAD-8A3167940575}.Debug|x64.ActiveCfg = Debug|x64
+		{1DEA8D1D-8BB9-4CC8-BBAD-8A3167940575}.Debug|x64.Build.0 = Debug|x64
+		{1DEA8D1D-8BB9-4CC8-BBAD-8A3167940575}.Debug|x86.ActiveCfg = Debug|Win32
+		{1DEA8D1D-8BB9-4CC8-BBAD-8A3167940575}.Debug|x86.Build.0 = Debug|Win32
+		{1DEA8D1D-8BB9-4CC8-BBAD-8A3167940575}.Release|x64.ActiveCfg = Release|x64
+		{1DEA8D1D-8BB9-4CC8-BBAD-8A3167940575}.Release|x64.Build.0 = Release|x64
+		{1DEA8D1D-8BB9-4CC8-BBAD-8A3167940575}.Release|x86.ActiveCfg = Release|Win32
+		{1DEA8D1D-8BB9-4CC8-BBAD-8A3167940575}.Release|x86.Build.0 = Release|Win32
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {551A7F8B-12E1-4136-8CA7-E75BBC9151E8}
+	EndGlobalSection
+EndGlobal
